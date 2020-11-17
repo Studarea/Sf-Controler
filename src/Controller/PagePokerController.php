5382a1d312c564d333de88cae154ed93d5ecd9db
Cyril <graf>
2020-11-17 12:30:21 +0100

    création d'une wildcard (maj)

--- src/Controller/PagePokerController.php
+++ src/Controller/PagePokerController.php
@@ -25,8 +25,8 @@ class PagePokerController
      *  possibles sur l'utilisateur (variables GET, POST... cookies)
      */
 
-public function poker(Request $request)
-{
+    public function poker(Request $request)
+    {
 
         // La classe Request de Symfony possède des méthodes et des propriétés
         // Pour les variables GET, il faut utiliser la propriété query.
@@ -34,7 +34,7 @@ public function poker(Request $request)
         // je créer une variable qui récupère les données de la requête :
         // http://localhost/sf-exe/public/poker?agepoker=18
 
-      $siAge = $request->query->get('agepoker');
+        $siAge = $request->query->get('agepoker');
 
         // Je créer une condition qui appelle ma variable $siAge et
         // vérifie si l'âge entré dans l'url est supérieur ou égal à 18 ans.
@@ -44,34 +44,32 @@ public function poker(Request $request)
             echo 'tu es majeur, Bienvenue tu peux jouer au POKER';
         } else {
             echo 'tu es mineur, désolé !!! retourne jouer sur l\'autoroute';
-
         }
-}
+        }
 
-/* EXO :
-- Créer une nouvelle page avec en paramètre d'url le nom et le prénom d'une personne
-- Récupérer ces params d'url dans une méthode de controleur
-- Renvoyer une réponse HTTP qui contient une balise HTML <p> avec à l'intérieur 'Bonjour' suivi du nom et du prénom présent dans l'url
-*/
+        /* EXO :
+        - Créer une nouvelle page avec en paramètre d'url le nom et le prénom d'une personne
+        - Récupérer ces params d'url dans une méthode de controleur
+        - Renvoyer une réponse HTTP qui contient une balise HTML <p> avec à l'intérieur 'Bonjour' suivi du nom et du prénom présent dans l'url
+        */
 
-    // pour plus de lisibilité, j'ai choisi de créer la @Route en annotations plutôt qu'en fichier YAML, XML, PHP.
-    // le chemin de l'url, son name (rendant ainsi unique cette @Route).
+        // pour plus de lisibilité, j'ai choisi de créer la @Route en annotations plutôt qu'en fichier YAML, XML, PHP.
+        // le chemin de l'url, son name (rendant ainsi unique cette @Route).
 
      /**
      * @Route("/prenomnom", name="prenom_nom")
      *
      */
 
-    // je créer une nouvelle méthode Reponse (visibilité de la propriété : public)
-    // pivate : concerne les atrributs et méthodes de la classe elle même.
-    // protected : concerne les attributs et méthodes communs (parente).
+        // je créer une nouvelle méthode Reponse (visibilité de la propriété : public)
+        // pivate : concerne les atrributs et méthodes de la classe elle même.
+        // protected : concerne les attributs et méthodes communs (parente).
 
-    // la classe Request de Symfony possède des méthodes et des propriétés
-    // pour récupérer les données de la requête.
+        // la classe Request de Symfony possède des méthodes et des propriétés
+        // pour récupérer les données de la requête.
 
 
     public function Reponse(Request $request)
-
     {
 
         // $request->query = permet de récupérer les paramètres de requête GET
@@ -90,17 +88,17 @@ public function poker(Request $request)
 
         return $response;
     }
-// http://localhost/sf-exe/public/prenomnom?prenom=Cyril&nom=PIERRE
+    
+        // http://localhost/sf-exe/public/prenomnom?prenom=Cyril&nom=PIERRE
 
 
 
 
-// exo :  Remplacer le système de paramètre d'url par le système de wildcard
+        // exo :  Remplacer le système de paramètre d'url par le système de wildcard
 
 
     /**
     * @Route("/article/{id}", name="article_show")
-     *
      *
      * je passe en paramètre de la méthode de controleur
      * la classe Request associée à la variable Request
@@ -110,43 +108,39 @@ public function poker(Request $request)
      */
 
 
-    // je créer la méthode articleShow et dans les paramètres ()
-    // (la classe Request associée à la variable Request)
+        // je créer la méthode articleShow et dans les paramètres ()
+        // (la classe Request associée à la variable Request)
 
-    public function articleShow($id) {
+    public function articleShow($id)
+    {
 
-    // var_dump( 'test'); die;
+        // var_dump( 'test'); die;
 
+        // j'utilise la classe Request pour récupérer le paramètre d'url et son id
+        // $idArticle = $request->query->get('id');
 
-    // j'utilise la classe Request pour récupérer le paramètre d'url et son id
-    // $idArticle = $request->query->get('id');
+        // je créer le tableau avec leur index numérique
+        // simulant une requète en BDD pour récupérer l'intégralité d'un article
 
+        $articles = [
+            1 => "Article 1",
+            2 => "Article 2",
+            3 => "Article 3",
+            4 => "Article 4",
+            5 => "Article 5",
+            6 => "Article 6",
+        ];
 
+        // je créer une réponse HTP contenant la valeur de l'article
+        // qui correspond à l'id passé en URL
 
-    // je créer le tableau avec leur index numérique
-    // simulant une requète en BDD pour récupérer l'intégralité d'un article
+        $response = new Response('<h1>'.$articles[$id].'</h1>');
 
-    $articles = [
-        '1' => "Article 1",
-        '2' => "Article 2",
-        '3' => "Article 3",
-        '4' => 'Article 4',
-        '5' => "Article 5",
-        '6' => "Article 6",
-    ];
-
-
-    // je créer une réponse HTP contenant la valeur de l'article
-    // qui correspond à l'id passé en URL
-
-    $response = new Response('<h1>'.$articles[$id].'</h1>');
-
-    // je retourne ma réponse
+        // je retourne ma réponse
 
         return $response;
 
-       //  http://localhost/sf-exe/public/article/2
-
+        //  http://localhost/sf-exe/public/article/2
     }
 }
 
